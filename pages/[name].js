1157a23f3f664074ab5923d0c46bd858f0724503
Gina A <70909035+gndz07@users.noreply.github.com>
2021-10-10 17:43:22 +0200

    feat: display issues statistic

--- pages/[name].js
+++ pages/[name].js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 import containerStyles from '../styles/Container.module.css'
 import styles from '../styles/RepoPage.module.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import Link from 'next/link'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 export async function getStaticPaths() {
     return {
@@ -20,11 +21,13 @@ export async function getStaticProps({ params }) {
     let myHeaders = new Headers();
     myHeaders.append('Authorization', `token ${process.env.API_TOKEN}`);
 
+    /*
     const collaboratorsRes = await fetch(`https://api.github.com/repos/${process.env.REPO_USERNAME}/${params.name}/collaborators`,
     {
         headers: myHeaders
     })
     const collaborators = await collaboratorsRes.json()
+    */
 
     const issuesRes = await fetch(`https://api.github.com/repos/${process.env.REPO_USERNAME}/${params.name}/issues`,
     {
@@ -41,18 +44,34 @@ export async function getStaticProps({ params }) {
     return {
       props: {
         repoData,
-        collaborators,
         issues
       },
     }
 }
 
-export default function RepoStatPage ({ repoData, collaborators, issues}) {
+export default function RepoStatPage ({ repoData, issues}) {
     const [displayedData, setDisplayedData] = useState(repoData);
+    const [issuesData, setIssuesData] = useState(issues);
 
     useEffect(() => {
         setDisplayedData(repoData);
     }, [repoData]);
+ 
+    const filteredIssues = useMemo(() => {
+        if (issues) {
+           const filtered = issues.filter((issue) => {
+                return issue.state === 'open'
+            })
+            return filtered.reverse();
+        }
+    }, [issues]);
+
+    useEffect(() => {
+        if (filteredIssues) {
+            const data = filteredIssues.map((issue, index) => ({ date: issue.created_at.split("T").slice(0,1), issues: index+1 }));
+            setIssuesData(data);
+        }
+    }, [filteredIssues]);
 
     return (
         <div className={containerStyles.container}>
@@ -75,8 +94,25 @@ export default function RepoStatPage ({ repoData, collaborators, issues}) {
                     <p className={styles.description}>{displayedData?.description}</p>
                 </div>
 
-                <p>{collaborators ? collaborators[0].login : ""}</p>
-                <p>{issues ? (issues.length > 0 ? issues[0].title : "") : ""}</p>
+                <div className={styles.graph_container}>
+                    <ResponsiveContainer width='100%' height='100%'>
+                    <LineChart
+                        data={issuesData}
+                        margin={{
+                        top: 20,
+                        right: 30,
+                        left: -10,
+                        bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="date" tick={false} tickLine={false} />
+                        <YAxis tick={false} tickLine={false} />
+                        <Tooltip />
+                        <Line type="monotone" dataKey="issues" stroke="#C2DEFB" dot={false} />
+                    </LineChart>
+                    </ResponsiveContainer>
+                </div>
             </main>
 
         </div>
